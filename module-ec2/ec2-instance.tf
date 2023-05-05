7b46e08b19448ad1e9b51c07d6ecf7b030914f7f
Adam Cahan <adam.cahan@gmail.com>
2023-05-05 10:48:45 -0500

    root module security group

--- module-ec2/ec2-instance.tf
+++ module-ec2/ec2-instance.tf
@@ -1,7 +1,4 @@
- resource "aws_instance" "app_server" {
-    ami = var.ami
- }
-
+# Variables we will use
  variable "ami" {
     type = string
     description = "holds ami image"
@@ -15,4 +12,45 @@
  variable "instance_type" {
     type = string
     default = "t2.micro"
+ }
+
+ # Create security group
+ resource "aws_security_group" "ec20neSG" {
+    name        = "ec2sg-test_web"
+    description = "Allow incoming HTTP connections & SSH access"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "${var.app_region}-sg"
+    }
+
+ }
+
+
+ # Create ec2 instance
+ resource "aws_instance" "app_server" {
+    ami = var.ami
+    instance_type = var.instance_type
+    vpc_security_group_ids = [ "${aws_security_group.ec20neSG.id}" ]
  }
